111c74fbb491abc54a8fddfd567319691f3ea798
torvalds <justice@JustIce.JustIce>
2024-06-30 15:02:17 -0500

    First commit

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'screen-recorder';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './manifest.json',
+    './Images/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
